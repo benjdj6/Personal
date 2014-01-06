ff9f86ef0adae288549ee6186ec99a9fb9d7388a
Ben Johnston <benjdj6@gmail.com>
2014-01-05 20:28:51 -0500

    Added Ruby on Rails project from Team Treehouse

---
+++ ruby/project1/odot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odot::Application.config.secret_key_base = 'd8ae4360ed656845a46fcf629cea419ba6b997e41e4c224016f68ec35ce48051f3cf23ba797df91caf9312843c4442d04150ad46127a1f4721758dc20c11f976'
